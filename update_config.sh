6ca93c7a22dd02e6b9d61942655c9631d843f04e
yatzima <justinmazk@gmail.com>
2024-09-26 22:45:17 +0200

    Create folder for all config files and update shell script

--- update_config.sh
+++ update_config.sh
@@ -1,4 +1,4 @@
-cp init.el ~/.emacs.d/init.el
-cp init.vim ~/.config/nvim/init.vim
-cp tmux.conf ~/.tmux.conf
+cp config/init.el ~/.emacs.d/init.el
+cp config/init.vim ~/.config/nvim/init.vim
+cp config/tmux.conf ~/.tmux.conf
 
