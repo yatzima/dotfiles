d069b4d88140ec507cfa7280159d0e67577adb96
yatzima <justinmazk@gmail.com>
2025-04-06 18:33:18 +0200

    Update Nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,13 +1,25 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "Yatzima's nix-darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, 
+  homebrew-cask, ... }:
   let
     configuration = { pkgs, config, ... }: {
 
@@ -25,6 +37,7 @@
           pkgs.obsidian
           pkgs.spotify
           pkgs.emacs
+          #pkgs.nerdfonts
         ];
 
       system.activationScripts.applications.text = let 
@@ -54,7 +67,8 @@
         dock.persistent-apps = [
           "/System/Applications/Mail.app"
           "/System/Applications/Calendar.app"
-          "/System/Applications/Safari.app"
+          #"/System/Applications/Safari.app"
+          "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app"
           "/System/Applications/Messages.app"
           "/System/Applications/Notes.app"
           "${pkgs.spotify}/Applications/Spotify.app"
@@ -68,6 +82,23 @@
         # Set default shell to alacritty
 
       };
+
+      homebrew = {
+        enable = true;
+        casks = [
+          "hammerspoon"
+          "google-chrome"
+          #"slack"
+          #"visual-studio-code"
+          #"font-jetbrains-mono-nerd-font"
+        ];
+        masApps = { 
+          "Dashlane" = 517914548;
+        };
+
+        #onActivation.cleanup = "zap";
+      };
+
       #fonts.packages = [
       #  (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
       #];
@@ -94,7 +125,36 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#simple
     darwinConfigurations."mini" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+        configuration 
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            #enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "justinma";
+
+            # Automatically migrate existing Homebrew installations
+            #autoMigrate = true;
+
+            # Optional: Declarative tap management
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+            };
+
+            # Optional: Enable fully-declarative tap management
+            #
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            mutableTaps = false;
+          };
+        }
+      ];
     };
   };
 }
