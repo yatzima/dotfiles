dac9489af879a497fbd1d82b7c40b85a7e255cfe
Justin Ma <justinmazk@gmail.com>
2026-06-07 23:41:19 +0200

    Fix noice.lua

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,22 +1,19 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	opts = {
-		notifier = { enabled = false },
-		input = { enabled = false },
-		scroll = { enabled = false },
-	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
 		require("noice").setup({
+			notifier = { enabled = false },
+			input = { enabled = false },
+			scroll = { enabled = false },
 			lsp = {
 				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 				override = {
 					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 				},
 			},
 			-- you can enable a preset for easier configuration
